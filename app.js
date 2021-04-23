4693d7f9315f4d1a3cdb5a8c206aee9b53e479e8
MuhammadZubair786 <mzhassan444@gmail.com>
2021-04-24 00:24:42 +0500

    Added Project Files

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+function textToAudio(){
+    let msg=document.getElementById("text").value;
+
+    let speech=new SpeechSynthesisUtterance();
+    speech.lang="en-US";
+
+    speech.text=msg;
+    speech.volume=20;
+    speech.rate=10;
+    speech.pitch=1;
+
+    window.speechSynthesis.speak(speech);
+
+ setTimeout(Clear, 2000);
+    
+}
+
+function Clear(){
+    document.getElementById('text').value="";
+}
