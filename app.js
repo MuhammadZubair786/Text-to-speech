11bb869f590eaf42bcffed3d1b0ca5b9e9fd1f18
MuhammadZubair786 <mzhassan444@gmail.com>
2021-04-24 00:30:16 +0500

    Added Project Files

--- app.js
+++ app.js
@@ -2,21 +2,30 @@
 
 function textToAudio(){
     let msg=document.getElementById("text").value;
+if(msg == ''){
+    alert("Enter Text For Convert To Spech")
+}
+else{
+
+
+  
 
     let speech=new SpeechSynthesisUtterance();
     speech.lang="en-US";
 
+
     speech.text=msg;
     speech.volume=20;
-    speech.rate=10;
+    speech.rate=1;
     speech.pitch=1;
 
     window.speechSynthesis.speak(speech);
 
- setTimeout(Clear, 2000);
+//  setTimeout(Clear, 2000);
     
 }
-
-function Clear(){
-    document.getElementById('text').value="";
 }
+
+// function Clear(){
+//     document.getElementById('text').value="";
+// }
